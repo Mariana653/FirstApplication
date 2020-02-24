cc0cdbbc585e5ba6faabf8b840cdf99de753435d
Cebotari  Mariana <makhu98@mail.ru>
2020-02-24 17:45:51 +0200

    Proiect 2

---
+++ Main.java
@@ -0,0 +1,49 @@
+public class Main{
+ 
+	public static void main(String[] args){
+          
+
+	     /* Here's an examples for primitive types (int) */
+             
+             int myExampleInteger1=10;
+             int myExampleInteger2;
+             myExampleInteger2=20;
+	     int myExampleInteger3=777+333;
+
+
+	     System.out.println("Example nr.1 for Integer type ="+myExampleInteger1);
+	     System.out.println("Example nr.2 for Integer type ="+myExampleInteger2);
+	     System.out.println("Example nr.3 for Integer type ="+myExampleInteger3);
+
+
+	     /* Here's an examples for primitive types (char) */
+             char myExampleChar1='A';
+	     char myExampleChar2;
+	     myExampleChar2=65;
+	     char myExampleChar3=55+10;
+
+	    
+	     System.out.println("Example nr.1 for char type ="+myExampleChar1);
+	     System.out.println("Example nr.2 for char type ="+myExampleChar2);
+	     System.out.println("Example nr.3 for char type ="+myExampleChar3);
+
+
+	     /* Here's an examples for primitive types (byte) */
+             byte myExampleByte1=25;
+	     byte myExampleByte2;
+	     myExampleByte2=15;
+	     byte myExampleByte3=110+15;
+
+
+	     System.out.println("Example nr.1 for byte type ="+myExampleByte1);
+	     System.out.println("Example nr.2 for byte type ="+myExampleByte2);
+	     System.out.println("Example nr.3 for byte type ="+myExampleByte3);
+
+
+
+	}
+           
+ 
+
+ 	
+}
