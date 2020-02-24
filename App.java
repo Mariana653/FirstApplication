1bdc8a8f465f3b0ea94145d4654d697d2db95c87
Cebotari  Mariana <makhu98@mail.ru>
2020-02-24 17:39:30 +0200

    Prima aplicatie

---
+++ App.java
@@ -0,0 +1,21 @@
+public class App{
+ 
+	public static void main(String[] args){
+          
+
+             /*Conversions*/
+             byte mbInKb  = (byte) 1024;
+	     int facebookFriends = (short) 150000;
+	     long starsInUniverse = (int) 15555555555555555L;
+	     double normalTemp = (long) 36.6;
+
+
+	     /*Display on screen*/	     
+	     System.out.println("Conversion byte to short ="+mbInKb);
+	     System.out.println("Conversion int to short ="+facebookFriends);
+             System.out.println("Conversion long to int ="+starsInUniverse);
+             System.out.println("Conversion double to long ="+normalTemp);
+
+	}  
+ 	
+}
